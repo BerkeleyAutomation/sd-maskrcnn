b378b320f2efcec3d12d329da0f492da4e89c4a0
Mike <michael.danielczuk@gmail.com>
2018-09-25 16:38:31 -0700

    dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@ requirements = [
     'mask-rcnn',
 ]
 
-with open('requirements.txt') as f:
-    requirements += f.read().splitlines()
-
 setup(name='sd_maskrcnn',
-      version='0.1.0',
-      description='SD Mask RCNN project code',
-      author='Michael Danielczuk',
-      author_email='mdanielczuk@berkeley.edu',
-      packages=['sd_maskrcnn'],
-      install_requires=requirements,
-     )
+    version='0.1.0',
+    description='SD Mask RCNN project code',
+    author='Michael Danielczuk',
+    author_email='mdanielczuk@berkeley.edu',
+    packages=['sd_maskrcnn'],
+    install_requires=requirements,
+)
