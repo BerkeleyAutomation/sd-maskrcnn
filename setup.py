497264f934aad75fd3d795f138523fdcc151b223
Mike <michael.danielczuk@gmail.com>
2019-05-01 16:02:20 -0700

    add plane urdf to data

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         'Topic :: Scientific/Engineering'
     ],
     packages=['sd_maskrcnn', 'sd_maskrcnn.envs'],
-    package_data={'sd_maskrcnn': ['data/*']},
+    package_data={'sd_maskrcnn': ['data/plane/*', 'data/bin/*']},
     setup_requires=setup_requirements,
     install_requires=requirements,
     extras_require={
