32adda0da498e616303a3a7d57234538d16d5357
Mike Danielczuk <michael.danielczuk@gmail.com>
2021-06-28 15:31:36 -0700

    add stereo camera capability, docker image

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+FROM nvidia/cudagl:11.1.1-devel-ubuntu20.04
+
+# env variables for tzdata install
+ARG DEBIAN_FRONTEND=noninteractive
+ENV TZ=America/Vancouver
+
+RUN apt-get update -y && \
+    apt-get install -y \
+        wget \
+        software-properties-common \
+        freeglut3-dev \
+        liboctomap-dev \
+        libfcl-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt-get install -y python3.8 python3.8-dev python3-pip
+
+# Install VHACD binary for convex decomp
+RUN set -xe
+RUN wget https://github.com/mikedh/v-hacd-1/raw/master/bin/linux/testVHACD
+RUN echo "e1e79b2c1b274a39950ffc48807ecb0c81a2192e7d0993c686da90bd33985130  testVHACD" | sha256sum --check
+RUN chmod +x testVHACD
+RUN mv testVHACD /usr/bin/
+
+# Install all other python deps
+RUN mkdir -p sd-maskrcnn
+COPY [ "./maskrcnn",  "sd-maskrcnn/maskrcnn" ]
+COPY [ "./setup.py",  "sd-maskrcnn/setup.py" ]
+RUN python3.8 -m pip install --no-cache-dir `python3.8 sd-maskrcnn/setup.py --list-setup`
+RUN python3.8 -m pip install --no-cache-dir `python3.8 sd-maskrcnn/setup.py --list-gen`
+
+# Install repo
+COPY [ ".",  "sd-maskrcnn/" ]
+RUN python3.8 -m pip install --no-cache-dir sd-maskrcnn/
+
+# Run generation
+WORKDIR sd-maskrcnn
+CMD PYOPENGL_PLATFORM=egl python3.8 tools/generate_mask_dataset.py /dataset --config_filename /cfg/generate_mask_dataset.yaml
--- setup.py
+++ setup.py
@@ -4,69 +4,80 @@ Setup of SD Mask RCNN codebase
 Author: Mike Danielczuk
 """
 
+import sys
 import os
 from setuptools import setup
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 
+setup_requirements = set(["Cython", "numpy", "nvidia-pyindex"])
+sub_requirements = set([f"mask-rcnn @ file://localhost{root_dir}/maskrcnn"])
+requirements = set(
+    [
+        "pycocotools>=2.0",  # For benchmarking
+        "scikit-image>=0.14.2",  # For image loading
+        "keras>=2.2<2.3",  # For training
+        "tqdm",  # For pretty progress bars
+        "matplotlib",  # For visualization of results
+        "h5py<3.0.0",  # Loading pretrained models
+        "autolab_core>=1.1.0",  # For core utilities
+        "nvidia-tensorflow",  # For training - need TF 1.15 so use nvidia
+    ]
+)
+
+generation_requirements = requirements.union(
+    [
+        "gym>=0.11",  # For sampling heaps
+        "pyglet==1.4.0b1",  # For pyrender
+        "pyrender>=0.1.23",  # For rendering images
+        "pybullet",  # For dynamic sim
+        "trimesh[easy]",  # For mesh loading/exporting
+        "scipy",  # For random vars
+    ]
+)
+
+# if someone wants to output a requirements file
+# `python setup.py --list-train > requirements.txt`
+# NOTE: maskrcnn lib must be installed separately
+if "--list-setup" in sys.argv:
+    print("\n".join(setup_requirements))
+    exit()
+elif "--list-train" in sys.argv:
+    print("\n".join(requirements))
+    exit()
+elif "--list-gen" in sys.argv:
+    print("\n".join(generation_requirements.union(requirements)))
+    exit()
+
 # load __version__
-version_file = 'sd_maskrcnn/version.py'
+version_file = "sd_maskrcnn/version.py"
 exec(open(version_file).read())
 
 # load README.md as long_description
-long_description = ''
-if os.path.exists('README.md'):
-    with open('README.md', 'r') as f:
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", "r") as f:
         long_description = f.read()
 
-setup_requirements = [
-    'Cython',                
-    'numpy'
-]
-
-requirements = [
-    'pycocotools>=2.0',         # For benchmarking
-    'scikit-image>=0.14.2',     # For image loading
-    'keras>=2.2<2.3',           # For training
-    'tqdm',                     # For pretty progress bars
-    'matplotlib',               # For visualization of results
-    'h5py<3.0.0',               # Loading pretrained models
-    'autolab_core>=1.1.0',      # For core utilities
-    'nvidia-tensorflow',        # For training - need TF 1.15 so use nvidia
-    f'mask-rcnn @ file://localhost{root_dir}/maskrcnn' 
-                                # Underlying Mask RCNN model
-]
-
-generation_requirements = [
-    'gym>=0.11',             # For sampling heaps
-    'pyglet==1.4.0b1',       # For pyrender  
-    'pyrender>=0.1.23',      # For rendering images
-    'pybullet',              # For dynamic sim
-    'trimesh[easy]',         # For mesh loading/exporting
-    'scipy'                  # For random vars
-]
-
 setup(
-    name='sd_maskrcnn',
+    name="sd_maskrcnn",
     version=__version__,
-    description='SD Mask RCNN project code',
+    description="SD Mask RCNN project code",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Michael Danielczuk',
-    author_email='mdanielczuk@berkeley.edu',
-    license='MIT',
-    url='http://github.com/BerkeleyAutomation/sd-maskrcnn',
+    long_description_content_type="text/markdown",
+    author="Michael Danielczuk",
+    author_email="mdanielczuk@berkeley.edu",
+    license="MIT",
+    url="http://github.com/BerkeleyAutomation/sd-maskrcnn",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        'Natural Language :: English',
-        'Topic :: Scientific/Engineering'
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
     ],
-    packages=['sd_maskrcnn', 'sd_maskrcnn.envs'],
-    package_data={'sd_maskrcnn': ['data/plane/*', 'data/bin/*']},
-    setup_requires=setup_requirements,
-    install_requires=requirements,
-    extras_require={
-        'generation': generation_requirements
-    }
+    packages=["sd_maskrcnn", "sd_maskrcnn.envs"],
+    package_data={"sd_maskrcnn": ["data/plane/*", "data/bin/*"]},
+    setup_requires=list(setup_requirements),
+    install_requires=list(requirements.union(sub_requirements)),
+    extras_require={"generation": list(generation_requirements)},
 )
